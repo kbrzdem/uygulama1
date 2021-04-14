1d95286859513e151e485b3988c5929fd6010941
kbrzdem <kbr_ozdm@hotmail.com>
2021-04-14 17:53:55 +0300

    düzenleme

--- react-local-storage/src/Components/form-data.js
+++ react-local-storage/src/Components/form-data.js
@@ -1,23 +1,55 @@
+import Form from './form'
+import React, { useState, useEffect } from "react";
+import {
+  useHistory
+} from 'react-router-dom'
 
-const FormData = ({ name, surname, handleSubmitForm, handleName, handleSurname }) => {
+const USERDATA = localStorage.getItem('userdata')
+  ? JSON.parse(localStorage.getItem('userdata'))
+  : [];
 
+const FormData = () => {
+  const [userdata, setUserdata] = useState(USERDATA);
+  const [name, setName] = useState('');
+  const [surname, setSurname] = useState('');
+  
+  const history = useHistory();
 
-    return (
-        <div className="container">
-            <form onSubmit={handleSubmitForm}>
-                <div className="form-group">
-                    <label>Name</label>
-                    <input type="text" className="form-control" value={name} onChange={handleName} />
-                </div>
-                <div className="form-group">
-                    <label>Surname</label>
-                    <input type="text" className="form-control" value={surname} onChange={handleSurname} />
-                </div>
-                <button type="submit" className="btn btn-primary btn-block" >Login</button>
-            </form>
-        </div>
-    );
+  const handleName = event => {
+    setName(event.target.value)
+  }
+  const handleSurname = event => {
+    setSurname(event.target.value)
+  }
 
-}
+  const handleSubmitForm = event => {
+    event.preventDefault();
+    if (name !== "" && surname !== "") {
+      const data = { name, surname }
+      setUserdata([...userdata, data])
+      setName('')
+      setSurname('')
+      history.push({ pathname: "/Home", state: { ...data }, id: Date.now() });
+    } else {
+      console.log("Hata")
+    }
+  }
+
+  useEffect(() => {
+    localStorage.setItem('userdata', JSON.stringify(userdata));
+  }, [userdata])
+
+  return (
+    <div className="container">
+      <Form
+        name={name}
+        surname={surname}
+        handleName={handleName}
+        handleSurname={handleSurname}
+        handleSubmitForm={handleSubmitForm} />
+    </div>
+  )
+
+};
 
 export default FormData;
